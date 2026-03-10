a9d1ca7de365c0fb4f01d5cec5b10e555529a09d
jonathanruvalcaba27 <johnnyruv2727@gmail.com>
2026-03-10 15:31:51 -0500

    Update my_irrigation_app.py

--- my_irrigation_app.py
+++ my_irrigation_app.py
@@ -1,3 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 st.set_page_config(layout="wide", initial_sidebar_state="expanded", page_title="Agricultural Irrigation Planner")
 
 
